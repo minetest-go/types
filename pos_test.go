1f7858bc79c6e27bde01297646fdccd7c47c0c3d
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-06-21 09:13:17 +0200

    add test for SortPos

--- pos_test.go
+++ pos_test.go
@@ -17,3 +17,16 @@ func TestPos(t *testing.T) {
 	assert.Equal(t, 20, p2.Y())
 	assert.Equal(t, 30, p2.Z())
 }
+
+func TestSortPos(t *testing.T) {
+	p1 := &types.Pos{1, 2, 3}
+	p2 := &types.Pos{3, 2, 1}
+
+	p1, p2 = types.SortPos(p1, p2)
+	assert.Equal(t, 1, p1[0])
+	assert.Equal(t, 2, p1[1])
+	assert.Equal(t, 1, p1[2])
+	assert.Equal(t, 3, p2[0])
+	assert.Equal(t, 2, p2[1])
+	assert.Equal(t, 3, p2[2])
+}
