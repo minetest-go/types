f486032fd7773c3493b6fd93a9c3847c9ac40176
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-09-02 12:49:14 +0200

    add `Area.String()`

--- area.go
+++ area.go
@@ -1,13 +1,19 @@
 package types
 
+import "fmt"
+
 type Area struct {
-	Pos1 *Pos
-	Pos2 *Pos
+	Pos1 *Pos `json:"pos1"`
+	Pos2 *Pos `json:"pos2"`
 }
 
 func NewArea(p1, p2 *Pos) *Area {
 	p1, p2 = SortPos(p1, p2)
 	return &Area{Pos1: p1, Pos2: p2}
+}
+
+func (a *Area) String() string {
+	return fmt.Sprintf("Area{%v,%v}", a.Pos1, a.Pos2)
 }
 
 // the inclusive size
